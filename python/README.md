649651065c82b26450e0ee6e1ccaf49562a07e69
Jacob <cmpute@qq.com>
2025-07-20 10:57:24 +0800

    feat(python): 添加Python和Zig的快速入门文档及构建配置

---
+++ python/README.md
@@ -0,0 +1,1 @@
+python msgpackpp quickstart
--- python/setup.py
+++ python/setup.py
@@ -1,4 +1,4 @@
-import os
+import os, shutil
 import subprocess
 import sys
 from pathlib import Path
@@ -7,11 +7,15 @@ from setuptools.command.build_ext import build_ext
 from distutils.sysconfig import get_python_inc, get_config_var
 
 class ZigExtension(Extension):
-    def __init__(self, name, sources):
+    def __init__(self, name, package_path):
         super().__init__(name, sources=[])
+        self.package_path = package_path
 
-class build_zig(build_ext):
+class build_ext_zig(build_ext):
     def build_extension(self, ext):
+        if not isinstance(ext, ZigExtension):
+            return super().build_extension(ext)
+
         # get Python include and library path
         python_include = Path(get_python_inc())
         python_libs = Path(get_config_var('installed_base'), 'libs').resolve()
@@ -21,28 +25,20 @@ class build_zig(build_ext):
         # get source file locations
         curdir = Path(__file__).parent.resolve()
         
-        # 构建 zig 编译命令
+        # compose build command
         build_cmd = [
             'python', '-m', 'ziglang', 'build',
             f'-DPYTHON_INCLUDE_DIR={python_include}',
             f'-DPYTHON_LIBS_DIR={python_libs}'
         ]
-        subprocess.check_call(build_cmd)
+        subprocess.check_call(build_cmd, cwd=ext.package_path)
 
-        out_path = "zig-out/bin/msgpackpp_python.dll"
-        if os.path.exists(ext_path):
-            os.unlink(ext_path)
-        os.rename(out_path, ext_path)
+        out_path = Path(ext.package_path, "zig-out/bin/msgpackpp_python.dll").resolve()
+        shutil.copy(out_path, ext_path)
 
-# 定义扩展模块
-msgpackpp_ext = ZigExtension('msgpackpp', sources=['lib.zig'])
 
 setup(
-    name='msgpackpp',
-    version='0.1',
-    description='A MessagePack extension module',
-    ext_modules=[msgpackpp_ext],
-    cmdclass={
-        'build_ext': build_zig
-    }
+    name='msgpackpp', # other information is put in pyproject.toml
+    ext_modules=[ZigExtension('msgpackpp', './')],
+    cmdclass={ 'build_ext': build_ext_zig }
 )
